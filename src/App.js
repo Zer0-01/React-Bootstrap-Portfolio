19e4543e0806fa91408c0cc6c5a75d8f1d355c48
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-06-26 22:45:39 +0800

    feat: import Routes, and Route and About feat: import boostrap css feat: create component for About

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
+import About from './components/About';
 
 function App() {
   return (
     <Router>
       <div>
         <Header />
+        <Routes>
+          <Route path='/' element={<About />} />
+        </Routes>
       </div>
     </Router>
   );
