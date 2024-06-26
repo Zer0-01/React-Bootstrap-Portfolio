af607317d8114dedc8979ce37a4e88f6ddcbacaa
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-06-26 23:22:12 +0800

    feat: add image

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFileDownload, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 import '../style.css';
+import anasImage from "../assets/anas.png"; // Import the image file
+
 
 const About = () => {
     return (
@@ -13,7 +15,9 @@ const About = () => {
                         About Me
                     </h2>
                     <div className="profile-image-container">
-                        <img src="/user.png" alt="user pic" className="img-fluid rounded-circle profile-image" />
+                    <img src={anasImage}
+                            alt="geek pic"
+                            className="img-fluid rounded-circle profile-image" /> 
                     </div>
                     <p className="section-description mt-4">
                         I am a Bachelor of Computer Science (Multimedia) with Honours student from UPM, currently gaining practical experience as an intern in mobile development. I have hands-on experience with mobile development languages such as Dart and Swift. Additionally, I am proficient in mobile development frameworks including Flutter and SwiftUI.
